593026c171682a16243ac791d2e596f86bad93e3
Ashwini <ashwini.agrawal90@gmail.com>
2022-07-09 01:33:09 -0400

    made changes in morbidity API get method

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ from dieticianApi import morbidity, users, recipe
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(morbidity.MorbidityApi, '/morbidity')
+api.add_resource(morbidity.MorbidityApi,
+                     '/morbidity/MorbidityName=<morbidityName>',
+                     '/morbidity/MorbidityTestId=<morbidityTestId>',
+                     '/morbidity/MorbidityName=<morbidityName>&MorbidityTestId=<morbidityTestId>',
+                     '/morbidity'
+                     )
+
 #api.add_resource(users.UsersApi, '/users')
 #api.add_resource(recipe.RecipeApi, '/recipes')
 
